6d7832d49660fcbecc8fb1e02ab3e6c904dead8a
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-14 11:46:58 +0330

    feat: add basic redis support

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ using Coresrv;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddDistributedRedisCache(option =>
+	{
+		option.Configuration = "127.0.0.1";
+		option.InstanceName = "master";
+	});
 
 var app = builder.Build();
 
--- Services/CoreService.cs
+++ Services/CoreService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Distributed;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using GSA.Services;
@@ -11,10 +12,24 @@ public class CoreService : GSA.Services.CoreService.CoreServiceBase
 {
     private readonly ILogger<CoreService> _logger;
 
-    public CoreService(ILogger<CoreService> logger) => _logger = logger;
+  	private readonly IDistributedCache _distributedCache;
+
+    public CoreService(
+        ILogger<CoreService> logger,
+        IDistributedCache distributedCache
+    ) {
+        _logger = logger;
+        _distributedCache = distributedCache;
+    }
 
     public override async Task<BulkStoreGameSalesReply> BulkStoreGameSales(BulkStoreGameSalesRequest request, ServerCallContext context)
     {
+        var prevId = await _distributedCache.GetStringAsync("prev_id");
+        if (string.IsNullOrEmpty(prevId)) {
+            prevId = Nanoid.Nanoid.Generate(size: 64);
+            await _distributedCache.SetStringAsync("prevId", prevId);
+        }
+
         var reply = new BulkStoreGameSalesReply { };
         reply.Items.Add(
             new GameSale
