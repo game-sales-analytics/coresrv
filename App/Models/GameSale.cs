1a9975372bb2150e1b185d3d2c4b8d2d9f5dbd8e
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-15 19:46:43 +0330

    feat: add GameSale app model

---
+++ App/Models/GameSale.cs
@@ -0,0 +1,34 @@
+using System;
+
+
+namespace App.Models
+{
+    public record GameSale
+    {
+        public float EuSales { get; init; }
+
+        public string Genre { get; init; }
+
+        public float GlobalSales { get; init; }
+
+        public float OtherSales { get; init; }
+
+        public float JpSales { get; init; }
+
+        public string Id { get; init; }
+
+        public string Name { get; init; }
+
+        public ulong Rank { get; init; }
+
+        public string Publisher { get; init; }
+
+        public uint Year { get; init; }
+
+        public DateTime RegisteredAt { get; init; }
+
+        public string Platform { get; init; }
+
+        public float NaSales { get; init; }
+    }
+}
