b826a2bade90d3e403b8f6e032db42ae58dd66a0
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-15 19:48:14 +0330

    feat: initialize database connection on app startup

--- App/Program.cs
+++ App/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using App;
 using App.Extensions;
+using App.DB;
+using App.DB.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +13,22 @@ builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddRedis(builder.Configuration);
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<GameSalesContext>(options =>
+{
+    options
+    .UseNpgsql(PostgreSQL.BuildPostgreSQLConnectionString(builder.Configuration))
+    .UseSnakeCaseNamingConvention();
+});
+builder.Services.AddSingleton<GameSalesRepository>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    services.GetRequiredService<GameSalesContext>().Database.EnsureCreated();
+}
 
 app.MapGrpcService<MainService>();
 
