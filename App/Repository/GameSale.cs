b0fc1f7a24509a9a16af27fec57639b373503ffd
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-17 12:51:44 +0330

    fix: handle duplicate game sales record execption

--- App/Repository/GameSale.cs
+++ App/Repository/GameSale.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using App.DB.Models;
 
 namespace App.DB.Repository
 {
@@ -9,14 +13,18 @@ namespace App.DB.Repository
     {
         private readonly GameSalesContext _context;
 
+        private readonly ILogger<GameSalesRepository> _logger;
+
         public GameSalesRepository(
-            GameSalesContext context
+            GameSalesContext context,
+            ILogger<GameSalesRepository> logger
         )
         {
             _context = context;
+            _logger = logger;
         }
 
-        public async Task SaveBulkGameSalesAsync(IEnumerable<App.Models.GameSale> gameSales)
+        public async Task SaveBulkGameSalesAsync(IEnumerable<App.Models.GameSale> gameSales, CancellationToken ct)
         {
             var temp = gameSales.Select(g => new App.DB.Models.GameSale
             {
@@ -34,21 +42,52 @@ namespace App.DB.Repository
                 Platform = g.Platform,
                 NaSales = g.NaSales,
             });
+            await _context.GameSales.AddRangeAsync(temp, ct);
 
-            await _context.GameSales.AddRangeAsync(temp);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync(ct);
+            }
+            catch (DbUpdateException ex)
+            {
+                switch (ex.InnerException)
+                {
+                    case PostgresException exception when exception.Severity == "ERROR" && exception.SqlState == PostgresErrorCodes.UniqueViolation:
+                        throw new DuplicateRecordException();
+                }
+
+                throw ex;
+            }
         }
 
-        public async Task<IEnumerable<Models.GameSale>> SearchGameByName(string name)
+        public async Task<IEnumerable<Models.GameSale>> SearchGameByName(string name, CancellationToken ct)
         {
             return await _context.GameSales
+                .AsNoTracking()
                 .Where(g => EF.Functions.ToTsVector("english", g.Name).Matches(name))
-                .ToListAsync();
+                .ToListAsync(ct);
         }
 
-        public async Task<IEnumerable<Models.GameSale>> GetGameSalesWithMoreSalesInEUThanNA()
+        public async Task<IEnumerable<Models.GameSale>> GetGameSalesWithMoreSalesInEUThanNA(CancellationToken ct)
         {
-            return await _context.GameSales.Where(g => g.EuSales > g.NaSales).ToListAsync();
+            return await _context.GameSales.AsNoTracking().Where(g => g.EuSales > g.NaSales).ToListAsync(ct);
+        }
+
+        public async Task<GameSale?> GetGameSalesByRank(ulong rank, CancellationToken ct)
+        {
+            try
+            {
+                return await _context.GameSales.AsNoTracking().SingleOrDefaultAsync(g => g.Rank.Equals(rank), ct);
+            }
+            catch (System.ArgumentNullException)
+            {
+                return await Task.FromResult<GameSale?>(null);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                _logger.LogError(ex, "multiple games with the same rank found");
+                return null;
+            }
         }
     }
 }
--- App/Repository/IGameRepository.cs
+++ App/Repository/IGameRepository.cs
@@ -1,14 +1,18 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace App.DB.Repository
 {
     public interface IGameSalesRepository
     {
-        Task SaveBulkGameSalesAsync(IEnumerable<App.Models.GameSale> gameSales);
+        Task SaveBulkGameSalesAsync(IEnumerable<App.Models.GameSale> gameSales, CancellationToken ct);
 
-        Task<IEnumerable<Models.GameSale>> SearchGameByName(string name);
+        Task<IEnumerable<Models.GameSale>> SearchGameByName(string name, CancellationToken ct);
 
-        Task<IEnumerable<Models.GameSale>> GetGameSalesWithMoreSalesInEUThanNA();
+        Task<IEnumerable<Models.GameSale>> GetGameSalesWithMoreSalesInEUThanNA(CancellationToken ct);
+
+        Task<Models.GameSale?> GetGameSalesByRank(ulong rank, CancellationToken ct);
     }
 }
