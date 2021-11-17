160b954336f694560030755ec52b2843180fa5d5
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-17 07:08:28 +0330

    feat: add SearchGameSalesByName grpc call handler

--- App/Services/CoreService.cs
+++ App/Services/CoreService.cs
@@ -6,10 +6,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
 using Google.Protobuf.WellKnownTypes;
-using Google.Protobuf.Collections;
 using Grpc.Core;
 using GSA.Services;
 using App.DB.Repository;
+
 
 namespace App
 {
@@ -21,13 +21,13 @@ namespace App
 
         private readonly IConfiguration _configuration;
 
-        private readonly GameSalesRepository _repo;
+        private readonly IGameSalesRepository _repo;
 
         public MainService(
             ILogger<MainService> logger,
             IDatabase cache,
             IConfiguration configuration,
-            GameSalesRepository repo
+            IGameSalesRepository repo
         )
         {
             _logger = logger;
@@ -38,7 +38,7 @@ namespace App
 
         public override async Task<PingReply> Ping(PingRequest request, ServerCallContext context)
         {
-            return await Task.FromResult(new PingReply { Pong = true });
+            return await Task.FromResult(new PingReply { Pong = false });
         }
 
         public override async Task<BulkStoreGameSalesReply> BulkStoreGameSales(BulkStoreGameSalesRequest request, ServerCallContext context)
@@ -86,6 +86,37 @@ namespace App
             reply.Items.AddRange(replyItems);
 
             return await Task.FromResult(reply);
+        }
+
+        public override async Task<SearchGameSalesByNameReply> SearchGameSalesByName(SearchGameSalesByNameRequest request, ServerCallContext context)
+        {
+            var metas = context.RequestHeaders.GetAll("token").ToList();
+            if (metas.Count != 1)
+            {
+                Console.WriteLine("bad token meta data provided");
+                throw new RpcException(new Grpc.Core.Status(StatusCode.Unauthenticated, "bad credentials provided"));
+            }
+
+            var auth = new AuthService(_configuration["USERS_SERVICE_ADDRESS"], metas.First().Value);
+
+            var reply = new SearchGameSalesByNameReply { };
+            reply.Items.AddRange((await _repo.SearchGameByName(request.Name)).Select(item => new GameSale
+            {
+                EuSales = item.EuSales,
+                Genre = item.Genre,
+                GlobalSales = item.GlobalSales,
+                Id = item.Id,
+                JpSales = item.JpSales,
+                Name = item.Name,
+                NaSales = item.NaSales,
+                OtherSales = item.OtherSales,
+                Platform = item.Platform,
+                Publisher = item.Publisher,
+                Rank = item.Rank,
+                RegisteredAt = Timestamp.FromDateTime(item.RegisteredAt),
+                Year = item.Year,
+            }));
+            return reply;
         }
     }
 
