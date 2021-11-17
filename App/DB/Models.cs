56241b60fc669e26d845c91fd063131dbdf741b5
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-17 12:51:17 +0330

    fix: set default values for models to resolve compiler warning

--- App/DB/Models.cs
+++ App/DB/Models.cs
@@ -6,32 +6,32 @@ namespace App.DB.Models
 {
     public record GameSale
     {
-        public float EuSales { get; init; }
+        public float EuSales { get; init; } = default!;
 
-        public string Genre { get; init; }
+        public string Genre { get; init; } = default!;
 
-        public float GlobalSales { get; init; }
+        public float GlobalSales { get; init; } = default!;
 
-        public float OtherSales { get; init; }
+        public float OtherSales { get; init; } = default!;
 
-        public float JpSales { get; init; }
+        public float JpSales { get; init; } = default!;
 
-        public string Id { get; init; }
+        public string Id { get; init; } = default!;
 
-        public string Name { get; init; }
+        public string Name { get; init; } = default!;
 
-        public ulong Rank { get; init; }
+        public ulong Rank { get; init; } = default!;
 
-        public string Publisher { get; init; }
+        public string Publisher { get; init; } = default!;
 
-        public uint Year { get; init; }
+        public uint Year { get; init; } = default!;
 
-        public DateTime RegisteredAt { get; init; }
+        public DateTime RegisteredAt { get; init; } = default!;
 
-        public string Platform { get; init; }
+        public string Platform { get; init; } = default!;
 
-        public float NaSales { get; init; }
+        public float NaSales { get; init; } = default!;
 
-        public NpgsqlTsVector SearchVector { get; init; }
+        public NpgsqlTsVector SearchVector { get; init; } = default!;
     };
 }
